0da2696eab532c56c486ad5dcb55dcfe4f95af2f
David McColl <dmccoll@sdccd.edu>
2025-03-07 14:30:45 -0800

    add usrt logging

--- src/main.zig
+++ src/main.zig
@@ -9,11 +9,33 @@ const pin_config = rp2xxx.pins.GlobalConfiguration{
 };
 const pins = pin_config.pins();
 
+const uart = rp2xxx.uart.instance.num(0);
+const baud_rate = 115200;
+const uart_tx_pin = rp2xxx.gpio.num(0);
+const uart_rx_pin = rp2xxx.gpio.num(1);
+//const led = rp2xxx.gpio.num(25);
+
+pub const microzig_options = .{
+    .log_level = .debug,
+    .logFn = rp2xxx.uart.logFn,
+};
+
 pub fn main() !void {
+    inline for (&.{ uart_tx_pin, uart_rx_pin }) |pin| {
+        pin.set_function(.uart);
+    }
+    uart.apply(.{ .baud_rate = baud_rate, .clock_config = rp2xxx.clock_config });
+    rp2xxx.uart.init_logger(uart);
+    std.log.info("Hello Logger", .{});
+    // led.set_function(.sio);
+    // led.set_direction(.out);
     pin_config.apply();
 
+    var count: u32 = 0;
     while (true) {
         pins.led.toggle();
-        time.sleep_ms(1000);
+        std.log.info("toggle led {}", .{count});
+        count += 1;
+        time.sleep_ms(2000);
     }
 }
